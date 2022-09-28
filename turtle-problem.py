1421079a672b462733d17808a4fd5320ac788ace
MC14 <morgan.cowans@outlook.com>
2022-09-28 17:16:33 -0400

    new turtle code

---
+++ turtle-problem.py
@@ -0,0 +1,7 @@
+import turtle               # allows us to use the turtles library
+wn = turtle.Screen()        # creates a graphics window
+alex = turtle.Turtle()
+larry = turtle.Turtle()
+tess = turtle.Turtle()
+tess.color("orange")
+larry.color("blue")
