33617907dbe680911066616df68dcd1a63e670a1
MC14 <morgan.cowans@outlook.com>
2022-10-05 15:45:08 -0400

    turtle ex

---
+++ turtle_ex.py
@@ -0,0 +1,19 @@
+from re import X
+
+
+class turtle_ex:
+
+    def turtle_message(x):
+        print ("I am turtle")
+        print(X)
+
+    def forward(value):
+        print("moving turtle")
+        print(value)
+
+    def turn(value):
+        print("turning turtle")
+        print(value)
+
+
+
