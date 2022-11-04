2547a0e54fb783f25d7ff862e30d207ea85cd477
MC14 <morgan.cowans@outlook.com>
2022-11-04 14:34:22 -0400

    added month_statement and made it a method. added variables for net profit calculation

--- Assignment 2/Assignment 2.py
+++ Assignment 2/Assignment 2.py
@@ -1,4 +1,6 @@
+import random
 class Product:
+   
     def __init__(self,code,name,price,manu_cost,stock,month_units):
         print("*** Morgan's Sample Stock Statement ***")
         print("Product Code:",code,)
@@ -8,19 +10,35 @@ class Product:
         print("Manufacture Cost:",manu_cost,"CAD")
         print("Monthly Production:",month_units,"units (Approx.)")
    
+    def month_Statement(self,month_units,stock,price,manu_cost):
+        for month in range(1,13):
+            print("Month",month,":")
+            print("Manufactured:",month_units,"units")
+            new_sold = random.randrange(-10,11)
+            Sold = int(month_units) + new_sold
+            print("Sold:",Sold,"units")
+            new_stock = int(stock) + (-new_sold)
+            print("Stock:",new_stock,"units")
+            total_Manufactured = total_Manufactured + month_units
+            total_Unit_Sold = total_Unit_Sold + new_sold
+        
+        net_Profit = (total_Unit_Sold * price ) - (total_Manufactured * manu_cost)
+        print(net_Profit)
+   
    
  
-the_loop = False
+
  
 class Application:
- 
-   
+    the_loop = 0
     print("Welcome to Morgan's Magnificent Products")
-    while the_loop == False:
+    while the_loop == 0:
         code = input("Please enter your Product's code: ")
         code = int(code)
         if code >= 100 and code <= 1000:
-            the_loop == True
+            print(type(code))
+            print("Hooray")
+            the_loop == 1
         else:
             print("Invalid Product Code, please try again")
  
@@ -30,4 +48,6 @@ class Application:
     manu_cost = input("Please enter the Manufacturing Cost: ")
     stock = input("Please enter the amount of Stock of the Product: ")
     month_units = input("Please enter the amount of stock made in a month: ")
-    App_product = Product(code,name,price,manu_cost,stock,month_units)
+    app_Product = Product(code,name,price,manu_cost,stock,month_units)
+    app_Product.month_Statement(month_units,stock,price,manu_cost)
+    
---
+++ class test.py
@@ -0,0 +1,11 @@
+class test:
+
+    def __init__(self, colour):
+       print("hello constructor")
+       self.colour = print("red")
+
+
+
+
+obj = test
+print(type(obj))
