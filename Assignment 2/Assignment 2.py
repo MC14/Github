f78f2904d2c873a5942d63aed1f8c5356f50a5e3
MC14 <morgan.cowans@outlook.com>
2022-11-05 18:31:14 -0400

    created other while loops for input statement, also more comments

--- Assignment 2/Assignment 2.py
+++ Assignment 2/Assignment 2.py
@@ -7,7 +7,7 @@ class Product: #Creates the class product, with the data given from the applicat
         print("Product Name:",name,) #prints the name 
  
         print("Sale Price:",price,"CAD") #prints the sale price
-        print("Manufacture Cost:",manu_cost,"CAD") #prints the manufacture cpst
+        print("Manufacture Cost:",manu_cost,"CAD") #prints the manufacture cost
         print("Monthly Production:",month_units,"units (Approx.)") #print the monthly production given
    
     def month_Statement(self,month_units,stock,price,manu_cost): #creates a method with these parameters variables to estimate a net profit within 12 months.
@@ -22,32 +22,56 @@ class Product: #Creates the class product, with the data given from the applicat
             total_Manufactured = total_Manufactured + month_units #variable that keeps track of total amount of product manufactured through all the months
             total_Unit_Sold = total_Unit_Sold + new_sold #variable that keeps track of total amount of product sold through all the months
         
-        net_Profit = (total_Unit_Sold * price ) - (total_Manufactured * manu_cost)
-        print(net_Profit)
+        net_Profit = (total_Unit_Sold * price ) - (total_Manufactured * manu_cost) #creates a variable that calculates the net profit of the 12 months
+        print(net_Profit) #prints net profit to show it
    
    
  
 
  
-class Application:
-    the_loop = 0
-    print("Welcome to Morgan's Magnificent Products")
+class Application: #creates a class that runs all the inputs in the program and then runs the Product class
+    the_loop = 0 #sets the_loop to 0 in order to use while loops
+    print("Welcome to Morgan's Magnificent Products") #print statement to show the start of the code
+    while the_loop == 0: #creates a while loop that continues until the_loop does not equal zero
+        code = input("Please enter your Product's code: ") #input statement to create the product's code
+        code = int(code) #turns the variable into an int if able, if not will cause an error
+        if code >= 100 and code <= 1000: #checks if the product code is in between 100 and 1000, if it is the code continues but if not, try again
+            the_loop = 1 #turns variable the_loop to 1 in order to break the while loop and continues the code
+        else: #if anything else, besides 100 and 1000
+            print("Invalid Product Code, please try again") #print invalid code and try again
+    the_loop = 0 #resets the_loop in order to do another while loop for the next input variable  
+    name = input("Please enter the Name of your Product: ")
     while the_loop == 0:
-        code = input("Please enter your Product's code: ")
-        code = int(code)
-        if code >= 100 and code <= 1000:
-            print(type(code))
-            print("Hooray")
+        price = input("Please enter the Price of the Product: ")
+        price = float(price) #turns the variable into a float
+        if price > 0:
             the_loop = 1
         else:
-            print("Invalid Product Code, please try again")
- 
-    loop = 0
-    name = input("Please enter the Name of your Product: ")
-    price = input("Please enter the Price of the Product: ")
-    manu_cost = input("Please enter the Manufacturing Cost: ")
-    stock = input("Please enter the amount of Stock of the Product: ")
-    month_units = input("Please enter the amount of stock made in a month: ")
-    app_Product = Product(code,name,price,manu_cost,stock,month_units)
-    app_Product.month_Statement(month_units,stock,price,manu_cost)
+            print("Invalid Price, please try again")
+    the_loop = 0
+    while the_loop == 0:
+        manu_cost = input("Please enter the Manufacturing Cost: ")
+        manu_cost = float(manu_cost) #turns the variable into a float
+        if manu_cost > 0:
+            the_loop = 1
+        else:
+            print("Invalid Manufacturing Cost, please try again")
+    the_loop = 0
+    while the_loop == 0:       
+        stock = input("Please enter the amount of Stock of the Product: ")
+        stock = int(stock)
+        if stock > 0:
+            the_loop = 1
+        else:
+            print("Invalid Stock, please try again")
+    the_loop = 0
+    while the_loop == 0:
+        month_units = input("Please enter the amount of stock made in a month: ")
+        month_units = int(month_units) #turns the variable into an interger
+        if month_units > 0:
+            the_loop = 1
+        else:
+            print("Invalid Monthly Units, please try again")
+    app_Product = Product(code,name,price,manu_cost,stock,month_units) #creates an object using the class product and the inputs given from the application class
+    app_Product.month_Statement(month_units,stock,price,manu_cost) #runs the Product class method using the object app_product and it's variables
     
